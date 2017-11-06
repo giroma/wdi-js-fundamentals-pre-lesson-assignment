9fea47dbb26e5bb8a44d27c50d279553a3dad30c
Argir Argirov <dj.giro@gmail.com>
2017-11-06 17:27:32 -0500

    for loop

--- pacman-console.js
+++ pacman-console.js
@@ -2,4 +2,12 @@
 
 var ghosts = {inky: 'Cyan', blinky: 'Red', pinky: 'Pink', clyde: 'Orange' };
 var weirdOne = 'inky';
+
+
 console.log(ghosts[weirdOne]);
+
+for (var index = 0; index < ghosts.length; index ++) {
+
+  console.log('Pac-Man eats ' + ghosts[index]);
+}
+ 
